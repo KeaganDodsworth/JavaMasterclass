ba306b296b558a8a31288e6c2d1db3a480edd9ae
Keagan Dodsworth <keagan.dods@gmail.com>
2021-10-25 14:46:48 +0200

    update

--- InterfacesChallenge/src/academy/learnprogramming/ISaveable.java
+++ InterfacesChallenge/src/academy/learnprogramming/ISaveable.java
@@ -1,10 +1,8 @@
 package academy.learnprogramming;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ISaveable {
-
     List<String> write();
     void read(List<String> savedValues);
 
--- InterfacesChallenge/src/academy/learnprogramming/Player.java
+++ InterfacesChallenge/src/academy/learnprogramming/Player.java
@@ -3,49 +3,49 @@ package academy.learnprogramming;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player implements ISaveable{
+public class Player implements ISaveable {
     private String name;
     private int hitPoints;
     private int strength;
     private String weapon;
 
-    public Player(String name, int hitPoints, int strength, String weapon) {
+    public Player(String name, int hitPoints, int strength) {
         this.name = name;
         this.hitPoints = hitPoints;
         this.strength = strength;
         this.weapon = "Sword";
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setHitPoints(int hitPoints) {
-        this.hitPoints = hitPoints;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public void setWeapon(String weapon) {
-        this.weapon = weapon;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getHitPoints() {
         return hitPoints;
     }
 
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
     public int getStrength() {
         return strength;
     }
 
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
     public String getWeapon() {
         return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
     }
 
     @Override
@@ -61,22 +61,36 @@ public class Player implements ISaveable{
     @Override
     public List<String> write() {
         List<String> values = new ArrayList<String>();
-        values.add(0,this.name);
+        values.add(0, this.name);
         values.add(1, "" + this.hitPoints);
         values.add(2, "" + this.strength);
-        values.add(3, "" + this.weapon);
+        values.add(3, this.weapon);
 
-        return null;
+        return values;
     }
 
     @Override
     public void read(List<String> savedValues) {
-        if(savedValues!= null && savedValues.size()>0){
+        if(savedValues != null && savedValues.size() >0) {
             this.name = savedValues.get(0);
             this.hitPoints = Integer.parseInt(savedValues.get(1));
             this.strength = Integer.parseInt(savedValues.get(2));
             this.weapon = savedValues.get(3);
-
         }
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
